5937fee65ff5f20f359953c958168a48caf0c0a6
unknown <aurelien@edumalin.fr>
2023-05-10 08:45:34 +0200

    diapo

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let count = 0;
+let translate = 0;
 fetch("./data.json")
     .then(result => result.json())
     .then(data => {
+        count = data.length;
         let dot = "";
         let diapo = "";
         for (let slide of data) {
@@ -18,6 +21,7 @@ fetch("./data.json")
                                 </div>
                             </div>
                         </div>
+                        <a class="subscribe" href="/inscription.html" target="_blank">Je participe</a>
                     </div>`;
                     dot += `<div class="dot"></div>`
         }
@@ -25,10 +29,23 @@ fetch("./data.json")
 
         document.querySelector(".diapo").innerHTML += `<div class="dot-list">${dot}</div>`;
 
-        document.querySelector(".diapo .slide").classList.add("active");
+        document.querySelector(".diapo .dot").classList.add("active");
         play();
     });
 
     function play(){
+        setInterval(()=>{
+            next();
+        },5000);
+    }
 
+    function next(){
+        if((count-1)*400 == translate){
+            translate = 0;
+        }else{
+            translate += 400;
+        }
+        document.querySelectorAll(".slide").forEach(slide => slide.style.transform = `translate(-${translate}px,0px)`);
+        document.querySelector(`.dot.active`).classList.remove("active");
+        document.querySelector(`.dot:nth-child(${translate/400+1})`).classList.add("active");
     }
